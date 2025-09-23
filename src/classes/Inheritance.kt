1e5ff37aa0c8480782671a9cadb2c4720665b751
Timothy Julian <timothyace73@gmail.com>
2025-09-23 20:13:35 +0700

    enhance User class with login status and override in Student class

--- src/classes/Inheritance.kt
+++ src/classes/Inheritance.kt
@@ -1,14 +1,21 @@
 package classes
 
 open class User (val name: String) {
-    fun login() {
+    open var isLoggedIn : Boolean = false
+    open fun login() {
         println("Inside user Login")
     }
 }
 
 
 // Kotlin can only extend 1 class
-class Student (name: String) : User(name)
+class Student (name: String) : User(name) {
+    override var isLoggedIn : Boolean = false
+    override fun login() {
+        println("Student login")
+        super.login()
+    }
+}
 
 class Instructor(name: String) : User(name)
 
@@ -16,6 +23,8 @@ fun main() {
     val student = Student("Tim")
     student.login()
     println("name is ${student.name}")
+    student.isLoggedIn = true
+    println("Logged in value is: ${student.isLoggedIn}")
 
     val instructor = Instructor("Jul")
     instructor.login()
