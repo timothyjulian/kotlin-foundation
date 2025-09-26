95e5ddab9e8ea1151b47ab5c6e1affc0bae365e7
Timothy Julian <timothyace73@gmail.com>
2025-09-26 21:58:53 +0700

    type checking and casting

---
+++ src/cast/ExploreCast.kt
@@ -0,0 +1,32 @@
+package cast
+
+import classes.Course
+
+fun main() {
+    val course = Course(1, "Rust Programming", "Tim")
+
+    checkType(course)
+    checkType("DILIP")
+
+    castNumber(1.0)
+    castNumber(1.toDouble())
+
+
+}
+
+fun castNumber(any: Any) {
+    when(any) {
+        any as Double -> println("Value is double")
+    }
+}
+
+fun checkType(any: Any) {
+    when (any) {
+        is Course -> {
+            println(any)
+        }
+        is String -> {
+            println(any.lowercase())
+        }
+    }
+}
