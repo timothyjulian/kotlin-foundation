c7491b5e16f065ad2d054413150fe82ecab77244
Timothy Julian <timothyace73@gmail.com>
2025-09-27 17:17:31 +0700

    lambda

---
+++ src/collections/ExploreLambdas.kt
@@ -0,0 +1,17 @@
+package collections
+
+
+fun main() {
+    val addLambda = { x: Int -> x + x }
+
+    val addResult = addLambda(3)
+    println("addResult : $addResult")
+
+    val multiplyLambda = { x: Int, y: Int ->
+        println("x is $x and y is $y")
+        x * y
+    }
+
+    val multiplyResult = multiplyLambda(2, 3)
+    println("multiplyResult : $multiplyResult")
+}
