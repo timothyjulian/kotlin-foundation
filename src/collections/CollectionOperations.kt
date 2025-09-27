e54c93ed95713733a0dcd655cfd4484dacd793a3
Timothy Julian <timothyace73@gmail.com>
2025-09-27 20:56:18 +0700

    flatMap & hashMap

--- src/collections/CollectionOperations.kt
+++ src/collections/CollectionOperations.kt
@@ -2,6 +2,7 @@ package collections
 
 import dataset.Course
 import dataset.CourseCategory
+import dataset.KAFKA
 import dataset.courseList
 
 fun main() {
@@ -9,8 +10,67 @@ fun main() {
     val devPredicate = {course: Course -> course.category == CourseCategory.DEVELOPEMENT }
     val designPredicate = {course: Course -> course.category == CourseCategory.DESIGN }
 
+    val list = listOf(listOf(1, 2, 3), listOf(4, 5, 6))
+
+    val mapResult = list.map { outerList ->
+        outerList.map {
+            it.toDouble()
+        }
+    }
+
+    val flatMapResult = list.flatMap { outerList ->
+        outerList.map {
+            it.toDouble()
+        }
+    }
+
+
+//    println("mapResult : $mapResult")
+//    println("flatMapResult : $flatMapResult")
+
 //    exploreFilter(courseList, designPredicate)
-    exploreMap(courseList, designPredicate)
+//    exploreMap(courseList, designPredicate)
+//    val exploreFlatMap = exploreFlatMap(courseList, KAFKA)
+//    println(exploreFlatMap)
+
+    exploreHashMap()
+}
+
+fun exploreHashMap() {
+    val nameAgeMutableMap = mutableMapOf("Tim" to 24, "Yenaaaaaaa" to 9)
+
+    nameAgeMutableMap.forEach { (k, v) ->
+        println("Key: $k and the value $v")
+    }
+
+    val value = nameAgeMutableMap.getOrElse("Tim1") { "asdasd" }
+    println("Value is $value")
+
+    val containsKey = nameAgeMutableMap.containsKey("abc")
+    println("result is $containsKey")
+
+    val filteredMap = nameAgeMutableMap
+        .filterKeys { it.length > 5 }
+        .map { it.key.uppercase() }
+    println("filteredMap is $filteredMap")
+
+    val maxAge = nameAgeMutableMap.maxByOrNull { it.value }
+    println("maxAge $maxAge")
+}
+
+fun exploreFlatMap(courseList: MutableList<Course>, kafka: String) : List<String> {
+    val kafkaCourses = courseList.flatMap { course ->
+        val courseName = course.name
+        val map = course.topicsCovered.filter {
+            it == kafka
+        }.map {
+            courseName
+        }
+        println(map)
+        map
+    }
+
+    return kafkaCourses
 }
 
 fun exploreMap(courseList: MutableList<Course>, predicate: (Course) -> Boolean) {
