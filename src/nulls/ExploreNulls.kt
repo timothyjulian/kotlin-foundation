223b015a948bae5de38346d8328804b727470ac5
Timothy Julian <timothyace73@gmail.com>
2025-09-27 12:24:56 +0700

    conversion nullable to non nullable and vice versa

--- src/nulls/ExploreNulls.kt
+++ src/nulls/ExploreNulls.kt
@@ -5,8 +5,18 @@ data class Movie(
     val name: String
 )
 
+fun printName(name: String) {
+    println("Name is $name")
+}
+
+fun printName1(name: String?) {
+    println("Name is $name")
+}
+
 fun main() {
     var nameNullable: String? = null;
+//    printName(nameNullable!!)
+    nameNullable?.run { printName(this) } // nullable to non nullable
     println("Value is : ${nameNullable?.length ?: 0}") // ? is safe operator
 
     nameNullable = "Test";
@@ -16,6 +26,7 @@ fun main() {
     println("Value is : $nameNullable")
 
     var name: String = "Dilip"
+    printName1(name) // non nullable to nullable
 
     val movie = Movie(null, "Avengers")
     println("Movie: $movie")
@@ -25,7 +36,7 @@ fun main() {
     println("Saved Movie: $savedMovie")
 }
 
-fun saveMovie(movie: Movie) : Movie {
-//    return movie.copy(id = 1)
-    return movie
+fun saveMovie(movie: Movie): Movie {
+    return movie.copy(id = 1)
+//    return movie
 }
