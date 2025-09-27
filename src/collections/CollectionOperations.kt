ff6d0b1e853ddb7686d74c91c889da19ecb9094f
Timothy Julian <timothyace73@gmail.com>
2025-09-27 20:28:24 +0700

    map operator

--- src/collections/CollectionOperations.kt
+++ src/collections/CollectionOperations.kt
@@ -9,7 +9,18 @@ fun main() {
     val devPredicate = {course: Course -> course.category == CourseCategory.DEVELOPEMENT }
     val designPredicate = {course: Course -> course.category == CourseCategory.DESIGN }
 
-    exploreFilter(courseList, designPredicate)
+//    exploreFilter(courseList, designPredicate)
+    exploreMap(courseList, designPredicate)
+}
+
+fun exploreMap(courseList: MutableList<Course>, predicate: (Course) -> Boolean) {
+    val courses = courseList
+        .filter(predicate)
+        .map { "${it.name} - ${it.category}" }
+        .forEach {
+            println(it)
+        }
+    println("courses: $courses")
 }
 
 fun exploreFilter(courseList: MutableList<Course>, predicate: (Course) -> Boolean) {
